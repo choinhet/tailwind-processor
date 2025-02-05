37500dc354e3320920c2d70de5f04747dbcd0545
rafael.choinhet <choinhet@gmail.com>
2025-02-05 09:50:39 -0300

    fix: manage temp file manually

--- tailwind_processor/tailwind_processor.py
+++ tailwind_processor/tailwind_processor.py
@@ -1,8 +1,9 @@
 import subprocess
-import tempfile
 import textwrap
 from pathlib import Path
 from typing import List
+import shutil
+
 
 import logging
 
@@ -18,54 +19,53 @@ class TailwindProcessor:
         """
         Process Tailwind classes into CSS.
         """
-        with tempfile.TemporaryDirectory() as temp_dir:
-            tailwind_apply = textwrap.dedent("""
-                @tailwind base;
-                @tailwind components;
-                @tailwind utilities;
+        tailwind_apply = textwrap.dedent("""
+            @tailwind base;
+            @tailwind components;
+            @tailwind utilities;
+        """)
+
+        parent = Path(__file__).parent / "tmp"
+        parent.mkdir(parents=True, exist_ok=True)
+
+        # All temporary files
+        input_file = parent / "input.css"
+        content_file = parent / "content.html"
+        configs = parent / "tailwind.config.js"
+
+        # Write the content file
+        tw_classes = " ".join(tailwind_classes)
+        content_file.write_text(f"<div class='{tw_classes}'></div>")
+
+        # Write the config file
+        config_content = (
+            textwrap.dedent("""
+            /** @type {import('tailwindcss').Config} */
+            module.exports = {
+                content: ['%s'],
+                theme: {
+                    extend: {},
+                },
+                plugins: [],
+            }
             """)
+        ) % content_file.as_posix()
 
-            parent = Path(temp_dir)
-            parent.mkdir(parents=True, exist_ok=True)
+        configs.write_text(config_content)
+        input_file.write_text(tailwind_apply)
+        base_task = "uv run tailwindcss".split(" ")
+        command = [
+            *base_task,
+            "-c", configs.as_posix(),
+            "-i", input_file.as_posix(),
+            "--minify",
+        ]
 
-            # All temporary files
-            input_file = parent / "input.css"
-            output_file = parent / "output.css"
-            content_file = parent / "content.html"
-            configs = parent / "tailwind.config.js"
+        result = subprocess.run(command, capture_output=True, text=True)
 
-            # Write the content file
-            tw_classes = " ".join(tailwind_classes)
-            content_file.write_text(f"<div class='{tw_classes}'></div>")
+        shutil.rmtree(parent.as_posix())
 
-            # Write the config file
-            config_content = (
-                textwrap.dedent("""
-                /** @type {import('tailwindcss').Config} */
-                module.exports = {
-                    content: ['%s'],
-                    theme: {
-                        extend: {},
-                    },
-                    plugins: [],
-                }
-                """)
-            ) % content_file.as_posix()
+        if result.stderr:
+            log.info(result.stderr)
 
-            configs.write_text(config_content)
-            input_file.write_text(tailwind_apply)
-            base_task = "uv run tailwindcss".split(" ")
-            command = [
-                *base_task,
-                "-c", configs.as_posix(),
-                "-i", input_file.as_posix(),
-                "-o", output_file.as_posix(),
-                "--minify",
-            ]
-
-            result = subprocess.run(command, capture_output=True, text=True)
-            if result.stderr:
-                log.info(result.stderr)
-
-            result = output_file.read_text()
-            return result
+        return result.stdout
